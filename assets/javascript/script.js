2694d1df8c2c612cc675caafbdd5907facebc5a6
Tony Gottschalk <tony.gottschalk2017@gmail.com>
2018-01-28 20:28:04 -0500

    Clean up JS
    Reduced to 30 lines of code. Good Copy.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,29 @@
-var pContainerHeight = $('.splash-box').height();
+alert("connected");
+
+var splashBox = $('.splash-box').height();
 
 $(window).scroll(function(){
 
-  var wScroll = $(this).scrollTop();
+  var scroll = $(this).scrollTop();
 
-  if (wScroll <= pContainerHeight) {
-    $('.logo').css({
-      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-    });
-  }
+  // Picture Boca Raton opacity
+  if (scroll > $('.large-window').offset().top - $(window).height()){
 
+    $('.large-window').css({'background-position':'center '+ (scroll - $('.large-window').offset().top) +'px'});
 
-  // Landing Elements
-  if(wScroll > $('.landing').offset().top - ($(window).height() / 1.2)) {
-
-    $('.landing').each(function(i){
-
-      setTimeout(function(){
-        $('.landing').eq(i).addClass('is-showing');
-      }, (700 * (Math.exp(i * 0.14))) - 700);
-    });
-
-  }
-
-
-  // Promoscope
-  if(wScroll > $('.large-window').offset().top - $(window).height()){
-
-    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
-
-    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+    var opacity = (scroll - $('.large-window').offset().top + 400) / (scroll / 5);
 
     $('.window-tint').css({'opacity': opacity});
-
   }
 
+  // Technology and Experience Floating Elements
 
-  // Floating Elements
+  if (scroll > $('.tech-exp').offset().top - $(window).height()){
 
-  if(wScroll > $('.tech-exp').offset().top - $(window).height()){
-
-    var offset = (Math.min(0, wScroll - $('.tech-exp').offset().top +$(window).height() - 350)).toFixed();
+    var offset = (Math.min(0, scroll - $('.tech-exp').offset().top +$(window).height() - 350)).toFixed();
 
     $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
 
     $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
-
   }
 });
-
-
