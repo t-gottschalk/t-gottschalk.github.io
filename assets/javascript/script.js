5534945f2323e1f16c006b91d860fec06d250f13
Tony Gottschalk <tony.gottschalk2017@gmail.com>
2018-01-31 16:17:34 -0500

    Adjusted navbar (Good Copy)
    Moved links to left. Adjusted jQuery scrollTop to display font color and navbar at 600px.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,20 @@
-// alert("connected");
-
 $(document).ready(function() {
 
 var splashBox = $('.splash-box').height();
 
 $(window).scroll(function() {
-
   var scroll = $(this).scrollTop();
   
   // Hidden Navbar
-  if (scroll > 600 && !$('header').hasClass('open') ) {
-    $('header').addClass('open');
-    $('header').slideDown();
-   } else if (scroll <= 600 ) {
-    $('header').removeClass('open');
-    $('header').slideUp();
+  if (scroll <= 600) {
+    $('header').css('background', 'transparent');
+    $('.nav-topics').css('color', 'white');
+   } else if (scroll > 600) {
+    $('header').css('background', 'black');
   }
 
   // SVG Logo image speed
   if (scroll <= splashBox) {
-
     $('.logo').css({
       'transform' : 'translate(0px, '+ scroll /1.3 +'%)'
     });
@@ -27,17 +22,13 @@ $(window).scroll(function() {
 
   // Picture Boca Raton opacity
   if (scroll > $('.large-window').offset().top - $(window).height()) {
-
     $('.large-window').css({'background-position':'center '+ (scroll - $('.large-window').offset().top) +'px'});
-
     var opacity = (scroll - $('.large-window').offset().top + 400) / (scroll / 5);
-
     $('.window-tint').css({'opacity': opacity});
   }
 
   // Technology and Experience Floating Elements
   if (scroll > $('.tech-exp').offset().top - $(window).height()) {
-
     var offset = (Math.min(0, scroll - $('.tech-exp').offset().top +$(window).height() - 350)).toFixed();
 
     $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
